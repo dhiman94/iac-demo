367fcb8a4ccb4f6507e2d91f45e4ed0bf3e353db
Dhiman Bhattacharya <39263213+dhiman94@users.noreply.github.com>
2020-09-06 21:41:43 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -11,3 +11,29 @@ resource "aws_vpc" "main" {
 output "vpc_id" {
   value = aws_vpc.main.id 
 }
+
+# create one s3 bucket for storing artifacts
+resource "aws_s3_bucket" "codedeploy_artifacts" {
+  bucket = "dhiman-codedeploy-artifacts"
+  acl    = "private"
+
+  lifecycle_rule {
+    id      = "delete_old_artifacts"
+    enabled = true
+
+    expiration {
+        days = 30
+    }
+
+  }
+
+  
+  tags = {
+    Name        = "Codedeploy Artifacts"
+    Environment = "Dev"
+  }
+}
+
+output "codedeploy_bucket_name" {
+    value =  aws_s3_bucket.codedeploy_artifacts.id 
+}  
