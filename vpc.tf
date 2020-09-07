ec353cf62314816c5d96c2cce441dfa70310c854
Dhiman Bhattacharya <39263213+dhiman94@users.noreply.github.com>
2020-09-07 14:28:28 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -34,6 +34,72 @@ resource "aws_s3_bucket" "codedeploy_artifacts" {
   }
 }
 
+# create bucket policy to allow ec2 codedeploy role
+resource "aws_s3_bucket_policy" "s3_codedeploy_policy" {
+  bucket = aws_s3_bucket.codedeploy_artifacts.id
+
+  policy = <<POLICY
+{
+     
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": ""arn:aws:iam::748658621424:role/codedeploy_service_role""
+            },
+            "Action": [
+                "s3:Get*",
+                "s3:List*",
+                "s3:PutObject"
+            ],
+            "Resource": "arn:aws:s3:::dhiman-codedeploy-artifacts/*"
+        }
+    ]
+   }
+   POLICY
+}
+
+
 output "codedeploy_bucket_name" {
     value =  aws_s3_bucket.codedeploy_artifacts.id 
 }  
+
+# create codedeploy application
+resource "aws_codedeploy_app" "spring_maven" {
+  compute_platform = "Server"
+  name             = "HelloWorldMaven"
+}
+
+# create codedeploy deployment group
+resource "aws_codedeploy_deployment_group" "spring_maven_deployment_group"{
+  app_name = aws_codedeploy_app.spring_maven.name
+  deployment_group_name = "HelloWorldMavenDeployGrp" 
+  service_role_arn      = "arn:aws:iam::748658621424:role/codedeploy_service_role"
+
+  deployment_style {
+    deployment_option = "WITHOUT_TRAFFIC_CONTROL"
+    deployment_type   = "IN_PLACE"
+  }
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
+
+  ec2_tag_set {
+    ec2_tag_filter {
+      key   = "Name"
+      type  = "KEY_AND_VALUE"
+      value = "Tomcat"
+    }
+
+    ec2_tag_filter {
+      key   = "Deployment"
+      type  = "KEY_AND_VALUE"
+      value = "Codedeploy"
+    }
+  }
+
+}
+
